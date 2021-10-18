7aa06d63bd9fdc9810b69be8a5bc499aeaaf357b
Nathan <suprdory@raspi>
2021-10-18 18:28:17 +0100

    import csv

---
+++ visualize.js
@@ -0,0 +1,23 @@
+import {csv} from "https://cdn.skypack.dev/d3-fetch@3";
+var container = document.getElementById('visualization');
+
+function plot(data){
+    // var dataset = new vis.DataSet(data);
+    var options = {
+        drawPoints: false,
+        interpolation: false,
+    // start: '2014-06-10',
+    // end: '2014-06-18'
+    };
+    // console.log(data)
+    var graph2d = new vis.Graph2d(container, data, options);
+}
+
+
+csv("/data/temp.csv").then(function(data) {
+    data.forEach(function(d) {
+    //   d.temp = +d.temp;
+    d.x=d.x.substring(0,19)
+    });
+    plot(data); 
+  });
