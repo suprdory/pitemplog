86d84b973bd4e43a8ea28adf1536811030c3fdbf
Nathan <suprdory@raspi>
2022-01-12 12:59:55 +0000

    tidy unused files

--- arx/process_templogs.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-dir=~/projects/webplotter/
-python3 ${dir}downsample_templog.py ${dir}temp.csv ${dir}temp_raspi_1min.csv
-python3 ${dir}downsample_templog.py ${dir}temp2.csv ${dir}temp_raspi2_1min.csv
-python3 ${dir}downsample_templog.py ${dir}temp0.csv ${dir}temp_pizero_1min.csv
-python3 ${dir}downsample_templog.py ${dir}temp3.csv ${dir}temp_audiopi_1min.csv
--- arx/pullcsvfrompis.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-dir=~/projects/webplotter/
-rsync suprdory@raspi2:projects/templog/temp2.csv ${dir}
-rsync suprdory@192.168.1.230:projects/templog/temp3.csv ${dir}
-#rsync suprdory@pizero:projects/templog/temp0.csv ${dir}
-${dir}process_templogs.sh
